9a6052832889314c2f16e2993385837518ff7f42
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-04-12 19:05:38 -0400

    add FindPaymentById use case and DTO for retrieving payment by ID

---
+++ src/contexts/payments/application/find-payment-by-id-use-case/find-payment-by-id.use-case.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '../../../shared/dependency-injection/injectable';
+import { PrimitivePayment } from '../../domain/payment';
+import { PaymentNotFoundException } from '../../domain/payment-not-found.exception';
+import { PaymentRepository } from '../../domain/payment.repository';
+import { FindPaymentByIdDto } from './find-payment-by-id.dto';
+
+@Injectable()
+export class FindPaymentByIdUseCase {
+  constructor(private readonly paymentRepository: PaymentRepository) {}
+
+  async execute(
+    findPaymentByIdDto: FindPaymentByIdDto,
+  ): Promise<{ payment: PrimitivePayment }> {
+    const payment = await this.paymentRepository.getById(findPaymentByIdDto.id);
+    if (payment) {
+      throw new PaymentNotFoundException(findPaymentByIdDto.id);
+    }
+    return {
+      payment: payment.toValue(),
+    };
+  }
+}
